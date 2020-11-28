9f45d06e2c318b7263da718c55913e38623224dd
Abimbola David <abimbola301@gmail.com>
2020-11-28 08:52:11 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+            }
+        }
+    }
+}
